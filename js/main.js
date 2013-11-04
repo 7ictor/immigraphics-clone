2a4ad921def15be25dade1dde4e0b9d962b5fead
7ictor <victor@rodriguezv.com>
2013-11-03 22:38:40 -0600

    filter search, displaying deaths along the map

--- js/main.js
+++ js/main.js
@@ -17,16 +17,20 @@ function initialize(lat, lng) {
     map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
     directionsDisplay.setMap(map);
-    var marker = new google.maps.Marker({
-        position: map.getCenter(),
-        map: map,
-        title: 'Your Address'
-    });
 }
 
 // Row builder for the JSON data response
 function rowBuilder(data, i){
-
+    return '<tr>'
+        + '<td>' + i + '</td>'
+        + '<td>' + data.Name + '</td>'
+        + '<td>' + data.Gender + '</td>'
+        + '<td>' + data['Report date'] + '</td>'
+        + '<td>' + data.Country + '</td>'
+        + '<td>' + data.Corridor + '</td>'
+        + '<td>' + data.Cod + '</td>'
+        + '<td>' + data['Ome cod'] + '</td>'
+    + '</tr>'
 }
 
 // Perform the Ajax request with the parameters
@@ -38,11 +42,36 @@ function ajax_request(parameters) {
         type:'GET',
         data: parameters,
         success:function(data){
-            console.log(data);
+            var cont = 0;
+            var currentInfoWindow = null;
             initialize(borderLat, borderLng);
-            /*$.each(data.results, function(i) {
-                $(rowBuilder(this, i)).appendTo("#results tbody")
-            });*/
+            $.each(data, function(i) {
+                cont++;
+                $(rowBuilder(this, cont)).appendTo("#results tbody")
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(this.Lat, this.Long),
+                    map: map,
+                    title: this.Name
+                });
+                marker.info = new google.maps.InfoWindow({
+                  content: '<b>Case ' + this['Ext id'] + '</b><br>'
+                    + this.Name + ', ' + this.Gender + '<br><br>'
+                    + '<b>Reporting Date:</b> '+  this['Report date'] + '<br>'
+                    + '<b>Corridor:</b> ' + this.Corridor + '<br>'
+                    + '<b>County:</b> ' + this.Country + '<br>'
+                    + '<b>Location:</b> ' + this.Place + '<br>'
+                    + '<b>Cause of Death:</b> ' + this.Cod + '<br>'
+                    + '<b>Description:</b> ' + this['Omd cod'] + '<br>'
+                });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    if (currentInfoWindow != null) {
+                        currentInfoWindow.close();
+                    }
+                    marker.info.open(map, marker);
+                    currentInfoWindow = marker.info;
+                });
+            });
         },
         error: function(error) {
             $("#request-error").show().html("<strong>Sorry!</strong> The request could not be processed due to a server error.");
@@ -52,12 +81,12 @@ function ajax_request(parameters) {
 
 $(function() {
     initialize(borderLat, borderLng);
-    var mydata = $("form#myform").serialize();
-    ajax_request(mydata);
-    $("form#myform").submit(function() {
+    $("form#search-form").submit(function() {
         $("#results tbody").find("tr").remove();
-        var mydata = $("form#myform").serialize();
-        ajax_request(mydata);
+        var mydata = $("#search-form").serialize();
+        //TODO: Fix the backend to exclude empty parameters
+        var cleandata = mydata.replace(/[^&]+=\.?(?:&|$)/g, '');
+        ajax_request(cleandata);
 
         return false;
     });
