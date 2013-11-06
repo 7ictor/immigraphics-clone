89db6d3c32c885d90257c64fd8086755e6c9abba
steelcolosus <eduardo.avilesj@gmail.com>
2013-11-06 14:51:44 -0600

    reorganizing methods inside controllers

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ var emmigraphics = angular.module('emmigraphics', [
   'emmigraphicsControllers'
 ]);
 
-emmigraphics.config(['$routeProvider',
-  function($routeProvider) {
+emmigraphics.config(['$routeProvider','$httpProvider',
+  function($routeProvider,$httpProvider) {
     $routeProvider.
       when('/', {
         templateUrl: 'partials/login.html',
@@ -18,10 +18,18 @@ emmigraphics.config(['$routeProvider',
         templateUrl: 'partials/login.html',
         controller: 'mainController'
       }).
+      when('/create', {
+        templateUrl: 'partials/create_user.html',
+        controller: 'createUserController'
+      }).      
       when('/submit', {
         templateUrl: 'partials/submit_case.html',
         controller: 'submitController'
       }).
+      when('/search', {
+        templateUrl: 'partials/search.html',
+        controller: 'searchController'
+      }).          
       when('/statistics', {
         templateUrl: 'partials/statistics.html',
         controller: 'statisticsController'
@@ -29,4 +37,12 @@ emmigraphics.config(['$routeProvider',
       otherwise({
         redirectTo: '/home'
       });
+
+      $httpProvider.defaults.useXDomain = true;
+      delete $httpProvider.defaults.headers.post['Content-type'];
+      delete $httpProvider.defaults.headers.common["X-Requested-With"];
+
+
   }]);
+
+
--- js/controllers.js
+++ js/controllers.js
@@ -6,18 +6,42 @@ var emmigraphicsControllers = angular.module('emmigraphicsControllers',[]);
 
 emmigraphicsControllers.controller('mainController',['$scope','$http',
 	function MainController($scope,$http){
-
-
+	    $scope.user={
+	    	name:'',
+	    	password:''
+	    };
 		$scope.Title = "login";
-		$http.get('json/states.json').success(function(data) {
-	      $scope.states = data;
-	      console.log(data);
+		// login function
+		$scope.login = function(){
+			var _username = $scope.user.name;
+			var _password = $scope.user.password;
+			var _passwordEnc = CryptoJS.MD5(_password).toString();
+			console.log(_username+": "+_passwordEnc);
+			$http({
+				method: 'POST',
+				url: 'http://safetrails.herokuapp.com/u/auth.php',
+				data: {
+					username:_username,
+					password:_passwordEnc
+				},
+				 withCredentials: false
+			}).success(function(data){
+				console.log(data);
+				if(data.response=="success"){
+		   			console.log("entro");
+		   			window.location.href="#/submit"
+		   		}else{
+		   			$(".message").html("Wrong username or password, please try again");
+		   			console.log("error");
+		   		}
+			});
+			
+		}
 
-	    });
 	}]);
 
 emmigraphicsControllers.controller('submitController',['$scope','$http',
-	function MainController($scope,$http){
+	function submitController($scope,$http){
 		$scope.Title = "Welcome, sumbit a case";
 		$http.get('json/countries.json').success(function(data) {
 	      $scope.countries = data;
@@ -27,8 +51,63 @@ emmigraphicsControllers.controller('submitController',['$scope','$http',
 	}]);
 
 emmigraphicsControllers.controller('statisticsController',['$scope','$http',
-	function MainController($scope,$http){
+	function statisticsController($scope,$http){
 		$scope.Title = "Statistics";
 
+
+	}]);
+
+
+emmigraphicsControllers.controller('searchController',['$scope','$http',
+	function searchController($scope,$http){
+		$scope.Title = "Search";
+
+
+	}]);
+
+
+emmigraphicsControllers.controller('createUserController',['$scope','$http',
+	function createUserController($scope,$http){
+		$scope.Title = "Search";
+		$http.get('json/states.json').success(function(data) {
+	      $scope.states = data;
+	    });
+
+		$scope.createUser = function(){
+			
+			var user = $scope.user;
+			var defaultUser={
+					   username:"",
+					   password:"",
+					   profession:"",
+					   state:"",
+					   county:"county_test",
+					   email:""
+					};
+			console.log(user.password+" : "+user.password2)
+			if(user.password!=user.password2){
+				alert("Passwords do not match");
+		    	return
+			}else{
+				user.password = CryptoJS.MD5(user.password).toString();
+				$http({
+					method: 'POST',
+					url: 'http://safetrails.herokuapp.com/u/save.php',
+					data: user,
+					withCredentials: false
+				}).success(function(data){
+					console.log(data);
+					if(data.response=="success"){
+			   			console.log("entro");
+			   			$(".message").html("User created");
+			   		}else{
+			   			$(".message").html("Something went Wrong");
+			   			console.log("error");
+			   		}
+				});
+			}
+			$scope.user=defaultUser;
+
+		}
 
 	}]);
